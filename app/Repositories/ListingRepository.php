7100cc242303c12f35067b0dc6ae98b69d9cb398
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2025-11-20 21:53:08 +0200

    Papildytas funkcionalumas

--- app/Repositories/ListingRepository.php
+++ app/Repositories/ListingRepository.php
@@ -13,6 +13,20 @@ class ListingRepository implements ListingRepositoryInterface
         return Listing::with(['user', 'category', 'listingPhoto'])->get();
     }
 
+    public function getPublic(): Collection
+    {
+        return Listing::where('statusas', '!=', 'parduotas')
+                  ->with(['user', 'category', 'listingPhoto'])
+                  ->get();
+    }
+
+    public function getByUser(int $userId): Collection
+{
+    return Listing::where('user_id', $userId)
+                  ->with(['category', 'listingPhoto'])
+                  ->get();
+}
+
     public function getById(int $id): ?Listing
     {
         return Listing::with(['user', 'category', 'listingPhoto'])->find($id);
