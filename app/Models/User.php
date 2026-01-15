c7da61e0f2350a1b6cfd2b44b5b1bd9e0914cc2c
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2026-01-15 08:11:49 -0800

    Naudoti autentifikaciją ir autorizacija

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     protected $fillable = [
         'vardas', 'pavarde', 'el_pastas', 'slaptazodis',
