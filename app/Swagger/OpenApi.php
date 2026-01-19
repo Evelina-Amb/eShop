253a3d2bb7d8aa202a69497c3f5e07a5377a3fc7
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2026-01-19 08:30:59 -0800

    Add files via upload

---
+++ app/Swagger/OpenApi.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Swagger;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\OpenApi(
+ *   @OA\Info(
+ *     title="eShop API",
+ *     version="1.0.0",
+ *     description="Automated API documentation for eShop project"
+ *   )
+ * )
+ */
+class OpenApi {}
